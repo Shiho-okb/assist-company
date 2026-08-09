1919b06bb0a2dca49403c3b0affecefdc74bae12
shiho_okabe <beeeeyan4@gmail.com>
2026-08-09 12:10:30 +0900

    Initial commit all themes

--- assist-company/page-recruit.php
+++ assist-company/page-recruit.php
@@ -1,5 +1,218 @@
 <?php get_header(); ?>
 
+<main>
+  <!-- ページヘッダー -->
+  <div class="p-recruit-header l-recruit-header c-page-header">
+    <div class="c-page-header__inner l-inner">
+      <div class="c-page-header__wrapper">
+        <h1 class="c-page-header__title c-title ">
+          <span class="c-title__en">Recruit</span>
+          <span class="c-title__ja">採用情報</span>
+        </h1>
+      </div>
+    </div>
+  </div>
 
+  <!-- パンくずリスト -->
+  <div class="p-news-breadcrumb c-breadcrumb">
+    <div class="c-breadcrumb__inner l-inner">
+      <ol class="c-breadcrumb__list">
+        <li class="c-breadcrumb__item">
+          <a href="./index.html">
+            TOP
+          </a>
+        </li>
+        <li class="c-breadcrumb__item">
+          採用情報
+        </li>
+      </ol>
+    </div>
+  </div>
+
+  <section class="p-recruit">
+    <div class="p-recruit__inner l-inner">
+      <div class="p-recruit__wrapper">
+        <div class="p-recruit__main">
+          <!-- 採用情報 -->
+          <div class="p-recruit__block">
+            <h2 class="p-recruit__heading">
+              採用職種
+            </h2>
+            <ul class="p-recruit__list">
+              <li class="p-recruit__item">
+                <p class="p-recruit__item-title">
+                  建築技術系
+                </p>
+                <p class="p-recruit__text">
+                  <span>設計図面の中の建物を現実の形に造りあげるのが施工管理の仕事です。</span>
+                  <span>プロジェクトの段階によっては事務所での作業もあります。</span>
+                  <span>工事が始まる前には協力会社と打ち合わせを重ね、施工計画やスケジュールを立てる内勤業務が多くなります。</span>
+                  <span>多くの関係者とコミュニケーションを取りながら、プロジェクト全体をスムーズに進める役割も担います。</span>
+                </p>
+              </li>
+              <li class="p-recruit__item p-recruit__item--facility">
+                <p class="p-recruit__item-title">
+                  建築設備（技術系）
+                </p>
+                <p class="p-recruit__text">
+                  <span>建築技術系は「プロジェクト協働」「高度技術への対応」「調査研究および技術開発」、BIM系ではBIM推進に向けた「環境整備、開発、展開」「BIM人財育成」「全社プロジェクト支援」「最先端情報の収集、分析、展開」といった業務となります。</span>
+                  <span>内勤が中心ですが、プロジェクトによっては技術検討・開発した事項を現場に常駐し管理する場面もあります。</span>
+                  <span>建築現場の改善には様々な専門分野の人財が、分野の垣根を越えて協働する役割を担います。</span>
+                </p>
+              </li>
+            </ul>
+          </div>
+          <!-- 入社後の推奨資格 -->
+          <div class="p-recruit__block">
+            <h2 class="p-recruit__heading">
+              入社後の推奨資格
+            </h2>
+            <ul class="p-recruit__list">
+              <li class="p-recruit__item">
+                <p class="p-recruit__item-title">
+                  建築コスト管理士
+                </p>
+              </li>
+              <li class="p-recruit__item">
+                <p class="p-recruit__item-title">
+                  施工管理技士
+                </p>
+              </li>
+              <li class="p-recruit__item">
+                <p class="p-recruit__item-title">
+                  建築積算士
+                </p>
+              </li>
+            </ul>
+            <p class="p-recruit__text">
+              <span>入社後の研修や業務での経験を通じて、必要なスキルや知識をしっかり学ぶことが重要になります。</span>
+              <span>専攻を問わず、成長意欲があり、何事にも道筋を立てて行動ができること、また、物事をやり遂げるという姿勢が必要不可欠です。</span>
+              <span>そのために、各年次で必要となるスキルを取得できる研修を用意しています。</span>
+              <span>希望者には海外研修や留学制度もあり、多様な経験を積みながらスキルアップを図る環境が整っています。</span>
+            </p>
+          </div>
+          <!-- 勤務地 -->
+          <div class="p-recruit__block">
+            <h2 class="p-recruit__heading">
+              勤務地
+            </h2>
+            <ul class="p-recruit__list">
+              <li class="p-recruit__item p-recruit__flex">
+                <p class="p-recruit__item-title">
+                  全国型：
+                </p>
+                <p class="p-recruit__text">
+                  本社・東京本店（東京）、大阪本店（大阪）、各支店および国内外の各事業所
+                </p>
+              </li>
+              <li class="p-recruit__item p-recruit__flex">
+                <p class="p-recruit__item-title">
+                  拠点型：
+                </p>
+                <p class="p-recruit__text">
+                  原則として、採用時に決定する各事業所の管轄地域内に限定
+                </p>
+              </li>
+            </ul>
+          </div>
+          <!-- 雇用条件 -->
+          <div class="p-recruit__block">
+            <h2 class="p-recruit__heading">
+              雇用条件
+            </h2>
+            <ul class="p-recruit__list">
+              <li class="p-recruit__item p-recruit__flex">
+                <p class="p-recruit__item-title">
+                  給与：
+                </p>
+                <p class="p-recruit__text">
+                  255,000円〜
+                </p>
+              </li>
+              <li class="p-recruit__item p-recruit__flex">
+                <p class="p-recruit__item-title">
+                  昇給：
+                </p>
+                <p class="p-recruit__text">
+                  年1回(4月)
+                </p>
+              </li>
+              <li class="p-recruit__item p-recruit__flex">
+                <p class="p-recruit__item-title">
+                  所定労働時間：
+                </p>
+                <p class="p-recruit__text">
+                  8:30〜17:15（7時間45分）
+                </p>
+              </li>
+              <li class="p-recruit__item p-recruit__flex">
+                <p class="p-recruit__item-title">
+                  休日・休暇：
+                </p>
+                <p class="p-recruit__text">
+                  完全週休2日制、夏期休日、年末・年始休日、結婚休暇、配偶者出産休暇、リフレッシュ休暇など
+                </p>
+              </li>
+              <li class="p-recruit__item p-recruit__flex">
+                <p class="p-recruit__item-title">
+                  保険：
+                </p>
+                <p class="p-recruit__text">
+                  健康保険、厚生年金保険、雇用保険、労災保険
+                </p>
+              </li>
+              <li class="p-recruit__item p-recruit__flex">
+                <p class="p-recruit__item-title">
+                  諸手当：
+                </p>
+                <p class="p-recruit__text">
+                  割増手当、別居手当、住宅手当、宿日直手当、特殊作業手当、海外勤務手当、通勤費、資格手当ほか
+                </p>
+              </li>
+              <li class="p-recruit__item p-recruit__flex">
+                <p class="p-recruit__item-title">
+                  福利厚生：
+                </p>
+                <p class="p-recruit__text">
+                  諸制度／社内預金、従業員持株会、退職年金、共済会など
+                </p>
+              </li>
+            </ul>
+          </div>
+          <!-- 画像 -->
+          <div class="p-recruit__images">
+            <div class="p-recruit__image">
+              <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/recruit/recruit_01.webp'); ?>" alt="施工スタッフ" width="316" height="380" loading="lazy">
+            </div>
+            <div class="p-recruit__image p-recruit__image--center">
+              <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/recruit/recruit_02.webp'); ?>" alt="打ち合わせ風景" width="316" height="380" loading="lazy">
+            </div>
+            <div class="p-recruit__image p-recruit__image--right">
+              <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/recruit/recruit_03.webp'); ?>" alt="現場管理" width="316" height="380" loading="lazy">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- お問い合わせ -->
+  <section class="p-contact-cta">
+    <div class="p-contact-cta__inner l-inner">
+      <div class="p-contact-cta__wrapper">
+        <h2 class="p-contact-cta__title c-title">
+          <span class="c-title__en">Contact</span>
+          <span class="c-title__ja">お問い合わせ</span>
+        </h2>
+        <div class="p-contact-cta__btn">
+          <a href="./contact.html" class="c-btn">
+            MORE
+            <span></span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
 
 <?php get_footer(); ?>
