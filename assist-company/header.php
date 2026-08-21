1ea5bd7ae3e18dc63077711a592a2d796a7a4352
shiho_okabe <beeeeyan4@gmail.com>
2026-08-21 11:16:17 +0900

    ヘッダーナビゲーション修正

--- assist-company/header.php
+++ assist-company/header.php
@@ -12,14 +12,14 @@
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
   <header class="p-header l-header">
-    <?php if ( is_front_page() ) : ?>
-    <h1>
-      <a href="#" class="p-header__logo">CODO ASSIST</a>
-    </h1>
+    <?php if (is_front_page()) : ?>
+      <h1>
+        <a href="#" class="p-header__logo">CODO ASSIST</a>
+      </h1>
     <?php else : ?>
-    <div>
-      <a href="#" class="p-header__logo">CODO ASSIST</a>
-    </div>
+      <div>
+        <a href="#" class="p-header__logo">CODO ASSIST</a>
+      </div>
     <?php endif; ?>
     <button class="p-header__hamburger js-hamburger" aria-label="ハンバーガーボタン">
       <span></span>
@@ -29,22 +29,22 @@
     <nav class="p-header__nav js-drawer">
       <ul class="p-header__nav-list">
         <li class="p-header__nav-item">
-          <a href="#" class="p-header__nav-link p-header__nav-link--active">ホーム</a>
+          <a href="<?php echo esc_url(home_url('/')); ?>" class="p-header__nav-link <?php if (is_front_page() && !is_home()) echo 'p-header__nav-link--active'; ?>">ホーム</a>
         </li>
         <li class="p-header__nav-item">
-          <a href="./news/" class="p-header__nav-link">お知らせ</a>
+          <a href="<?php echo esc_url(home_url('/news/')); ?>" class="p-header__nav-link <?php if ((is_home() || is_singular('post') || is_category() || is_tag() || is_tax()) && get_post_type() !== 'works' && !is_post_type_archive('works')) echo 'p-header__nav-link--active'; ?>">お知らせ</a>
         </li>
         <li class="p-header__nav-item">
-          <a href="./works/" class="p-header__nav-link">施工事例</a>
+          <a href="<?php echo esc_url(home_url('/works/')); ?>" class="p-header__nav-link <?php if (is_post_type_archive('works') || is_singular('works') || (is_tax() && get_post_type() === 'works')) echo 'p-header__nav-link--active'; ?>">施工事例</a>
         </li>
         <li class="p-header__nav-item">
-          <a href="./company/" class="p-header__nav-link">我々について</a>
+          <a href="<?php echo esc_url(home_url('/company/')); ?>" class="p-header__nav-link <?php if (is_page('company')) echo 'p-header__nav-link--active'; ?>">我々について</a>
         </li>
         <li class="p-header__nav-item">
-          <a href="./recruit/" class="p-header__nav-link">採用情報</a>
+          <a href="<?php echo esc_url(home_url('/recruit/')); ?>" class="p-header__nav-link <?php if (is_page('recruit')) echo 'p-header__nav-link--active'; ?>">採用情報</a>
         </li>
         <li class="p-header__nav-item">
-          <a href="./contact/" class="p-header__nav-link">お問い合わせ</a>
+          <a href="<?php echo esc_url(home_url('/contact/')); ?>" class="p-header__nav-link <?php if (is_page('contact')) echo 'p-header__nav-link--active'; ?>">お問い合わせ</a>
         </li>
       </ul>
     </nav>
