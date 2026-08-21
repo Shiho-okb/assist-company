7b019053abdf7c122b7b2df1b7f118a7a11864a1
shiho_okabe <beeeeyan4@gmail.com>
2026-08-21 11:22:36 +0900

    フッターナビゲーション修正

--- assist-company/footer.php
+++ assist-company/footer.php
@@ -2,22 +2,22 @@
   <nav class="p-footer__nav">
     <ul class="p-footer__nav-list">
       <li class="p-footer__nav-item">
-        <a href="#" class="p-footer__nav-link">ホーム</a>
+        <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__nav-link">ホーム</a>
       </li>
       <li class="p-footer__nav-item">
-        <a href="./news.html" class="p-footer__nav-link">お知らせ</a>
+        <a href="<?php echo esc_url(home_url('/news/')); ?>" class="p-footer__nav-link">お知らせ</a>
       </li>
       <li class="p-footer__nav-item">
-        <a href="./works.html" class="p-footer__nav-link">施工事例</a>
+        <a href="<?php echo esc_url(home_url('/works/')); ?>" class="p-footer__nav-link">施工事例</a>
       </li>
       <li class="p-footer__nav-item">
-        <a href="./company.html" class="p-footer__nav-link">我々について</a>
+        <a href="<?php echo esc_url(home_url('/company/')); ?>" class="p-footer__nav-link">我々について</a>
       </li>
       <li class="p-footer__nav-item">
-        <a href="./recruit.html" class="p-footer__nav-link">採用情報</a>
+        <a href="<?php echo esc_url(home_url('/recruit/')); ?>" class="p-footer__nav-link">採用情報</a>
       </li>
       <li class="p-footer__nav-item">
-        <a href="./contact.html" class="p-footer__nav-link">お問い合わせ</a>
+        <a href="<?php echo esc_url(home_url('/contact/')); ?>" class="p-footer__nav-link">お問い合わせ</a>
       </li>
     </ul>
   </nav>
